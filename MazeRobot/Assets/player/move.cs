d16178fa40cff7d915697a3cd456fd79fa8bda98
Alban642 <66842827+Alban642@users.noreply.github.com>
2021-01-05 13:37:38 +0100

    deplacement player

---
+++ MazeRobot/Assets/player/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    protected Joystick joy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joy = FindObjectOfType<Joystick>();
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * joy.Vertical/10);
+        transform.Translate(Vector3.right * joy.Horizontal / 10);
+    }
+}
